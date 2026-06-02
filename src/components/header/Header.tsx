0061a6bb6cedbb0c4f0e24d07adc9212be194a97
EvilDron09 <andrej32118@gmail.com>
2026-06-02 23:13:20 +0300

    Search

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,9 +3,23 @@ import { useState} from "react";
 import {GenreBadgeRender} from "../genres-badge-folder/genres-badge-render/GenreBadgeRender.tsx";
 import {UserInfo} from "../user-info/UserInfo.tsx";
 import {useNavigate} from "react-router-dom";
+
+
+
+
 export const Header = () => {
    const [openGenres, setOpenGenres] = useState(false);
    const toggleGenres = () => setOpenGenres(!openGenres);
+
+   const [searchQuery, setSearchQuery] = useState('');
+   const handleSearchSubmit = () =>{
+
+
+       if(searchQuery.trim()){
+           navigate(`/movieInfo/${encodeURIComponent(searchQuery.trim())}`);
+           setSearchQuery('')
+       }
+   }
 
    const navigate = useNavigate();
     const toGoMovieList = () => {
@@ -17,17 +31,20 @@ export const Header = () => {
             <UserInfo/>
             <h1><button onClick={toGoMovieList}>React Exam</button></h1>
             <div>
-            <div style={{position: 'relative', display: 'inline-block'}}>
-                <button onClick={toggleGenres} >Genres</button>
-                {openGenres && (
-                    <div style={{position: "absolute"} }>
-                        <GenreBadgeRender/>
-                        </div>
-                )}
+                <div className={"genres"}>
+                    <button onClick={toggleGenres} >Genres</button>
+                    {openGenres && (
+                        <div>
+                            <GenreBadgeRender/>
+                            </div>
+                    )}
+                </div>
+                <form onSubmit={handleSearchSubmit}>
+                    <input type="text" value={searchQuery} placeholder={"movie search"} onChange={(e) =>
+                        setSearchQuery(e.target.value)}/>
+                </form>
             </div>
 
-
-            </div>
 
 
         </header>
